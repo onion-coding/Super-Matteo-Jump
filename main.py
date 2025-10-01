f42940ee615798ad78ebe74c563a42ccef9f18c2
onion <sergiuhanganu00@gmail.com>
2025-10-02 00:08:46 +0300

    added some obstacle generation logic and fixed platform generation

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ TILE_SPEED = 8
 GRAVITY = 0.7
 PLAYER_JUMP_SPEED = 17
 
+class Player():
+    def __init__(self):
+        pass
+
 class GameView(arcade.Window):
     """
     Main application class.
@@ -33,6 +37,10 @@ class GameView(arcade.Window):
         self.player_texture = arcade.load_texture(
             "images/character/matteo_idle.png")
 
+        self._last_filename = None
+        self._repeat_count = 0
+
+        self._next_spawn_time = 0
         self._spawn_timer = 0
         self.difficulty = 1
         self.score = 1
@@ -100,22 +108,65 @@ class GameView(arcade.Window):
                 self.difficulty += 0.03 * delta_time
 
             self._spawn_timer += delta_time
-            if self._spawn_timer > 1:
-                print(self._spawn_timer)
+            if self._spawn_timer > self._next_spawn_time:
                 self._spawn_timer = 0
+                self._next_spawn_time = random.uniform(0.8, 2)
+
                 filename, scale, y = random.choice(self.obstacles_data)
+
+                if filename == self._last_filename:
+                    self._repeat_count += 1
+                    if self._repeat_count >= 3:
+                        while filename == self._last_filename:
+                            filename, scale, y = random.choice(self.obstacles_data)
+                        self._repeat_count = 0
+                else:
+                    self._repeat_count = 0
+                self._last_filename = filename
+
+                if filename == "bird.png":
+                    y = random.choice([80, 170, 230])
                 obs = arcade.Sprite("images/obstacles/" + filename, scale=scale)
                 obs.center_x = WINDOW_WIDTH + 50
                 obs.center_y = y
                 self.obstacle_list.append(obs)
+
+                if filename == "water.png":
+                    if self.difficulty < 2:
+                        block_count = random.randint(1, 3)
+                    elif self.difficulty < 3:
+                        block_count = random.randint(2, 4)
+                    elif self.difficulty < 3.49:
+                        block_count = random.randint(3, 5)
+                    else:
+                        block_count = random.randint(4, 6)
+
+                    print("blocks:", block_count)
+
+                    for i in range(block_count):
+                        obs = arcade.Sprite("images/obstacles/water.png", scale=scale)
+                        obs.center_x = WINDOW_WIDTH + 50 + (obs.width * i)
+                        obs.center_y = y
+                        self.obstacle_list.append(obs)
+
+                else:
+                    obs = arcade.Sprite("images/obstacles/" + filename, scale=scale)
+                    obs.center_x = WINDOW_WIDTH + 50
+                    obs.center_y = y
+                    self.obstacle_list.append(obs)
 
             for obs in self.obstacle_list:
                 obs.center_x -= TILE_SPEED * self.difficulty
 
             for platform in self.platform_list:
                 platform.center_x -= TILE_SPEED * self.difficulty
+
+            # check leftmost platform
+            rightmost = max(p.right for p in self.platform_list)
+            for platform in self.platform_list:
                 if platform.right < 0:
-                    platform.left = WINDOW_WIDTH
+                    platform.left = rightmost
+                    rightmost = platform.right
 
             collided = arcade.check_for_collision_with_list(self.player_sprite, self.obstacle_list)
             if collided:
@@ -129,6 +180,9 @@ class GameView(arcade.Window):
         if key == arcade.key.UP or key == arcade.key.W or key == arcade.key.SPACE:
             if self.physics_engine.can_jump():
                 self.player_sprite.change_y = PLAYER_JUMP_SPEED
+
+        if key == arcade.key.DOWN or key == arcade.key.S:
+            self.player_sprite.change_y = -PLAYER_JUMP_SPEED // 2
 
         if self.game_over:
             self.setup()
