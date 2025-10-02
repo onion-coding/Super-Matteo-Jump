0120d7d18825b97b38de8542e01bdde8d9bf03b7
onion <sergiuhanganu00@gmail.com>
2025-10-02 23:29:40 +0300

    added more obstacle logic, clouds, and jumping over bushes, game is fully playable at this point

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ WINDOW_HEIGHT = 360
 WINDOW_TITLE = "Super Matteo Jump"
 TILE_SCALING = 0.1
 TILE_SPEED = 8
+CLOUD_SPEED = 4
 GRAVITY = 0.7
 PLAYER_JUMP_SPEED = 17
 
@@ -27,10 +28,14 @@ class GameView(arcade.Window):
         self.player_list = None
         self.platform_list = None
         self.obstacle_list = None
+        self.cloud_list = None
+        self.house_list = None
         self.obstacle = None
         self.obstacles_data = None
         self.physics_engine = None
         self.game_over = False
+
+
 
     def setup(self):
 
@@ -55,17 +60,44 @@ class GameView(arcade.Window):
 
         self.platform_list = arcade.SpriteList(use_spatial_hash=True)
         self.obstacle_list = arcade.SpriteList(use_spatial_hash=True)
+        self.cloud_list = arcade.SpriteList()
+        self.house_list = arcade.SpriteList()
 
+        self.score_text = arcade.Text(
+            f"{int(self.score):05}",
+            WINDOW_WIDTH * 0.98,  # x
+            WINDOW_HEIGHT * 0.95,  # y
+            arcade.color.BLACK,
+            18,
+            anchor_x="right",
+            anchor_y="top"
+        )
+
+        # platforms
         for x in range(0, WINDOW_WIDTH+64, 64):
             platform = arcade.Sprite("images/tiles/tile_1.png", scale=TILE_SCALING)
             platform.center_x = x
             platform.center_y = 52
             self.platform_list.append(platform)
 
+        # clouds
+        for x in range(-800, 0, 200):  # start off-screen to the left
+            cloud = arcade.Sprite("images/background/cloud.png", scale=TILE_SCALING)
+            cloud.center_x = x
+            cloud.center_y = random.randint(150, 300)
+            self.cloud_list.append(cloud)
+
+        # houses
+        for x in range(-800, 0, 200):
+            house = arcade.Sprite("images/background/houses.png", scale=0.7)  # adjust scale if needed
+            house.center_x = x  # example x position
+            house.center_y = 100
+            self.house_list.append(house)
+
         self.obstacle_list = arcade.SpriteList()
         self.obstacles_data = [
             ("lawn-mower.png", TILE_SCALING * 1.7, 74),
-            ("water.png", TILE_SCALING, 52),
+            ("bushes-single.png", TILE_SCALING, 70),
             ("doggy.png", TILE_SCALING, 70),
             ("bird.png", TILE_SCALING, 170)
         ]
@@ -79,6 +111,8 @@ class GameView(arcade.Window):
     def on_draw(self):
         self.clear()
 
+        # self.house_list.draw()
+        self.cloud_list.draw()
         self.player_list.draw()
         self.platform_list.draw()
         self.obstacle_list.draw()
@@ -87,25 +121,19 @@ class GameView(arcade.Window):
             arcade.draw_text("GAME OVER", WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2,
                              arcade.color.RED, 50, anchor_x="center", anchor_y="center")
 
-        arcade.draw_text(
-            f"{int(self.score):05}",
-            WINDOW_WIDTH * 0.98,
-            WINDOW_HEIGHT * 0.95,
-            arcade.color.BLACK,
-            18,
-            anchor_x="right",
-            anchor_y="top"
-        )
+        self.score_text.draw()
 
     def on_update(self, delta_time):
 
         if not self.game_over:
             self.physics_engine.update()
 
+            self.score_text.text = f"{int(self.score):05}"
+
             self.score += delta_time * self.difficulty * 15
 
             if self.difficulty < 3.5:
-                self.difficulty += 0.03 * delta_time
+                self.difficulty += 0.01 * delta_time
 
             self._spawn_timer += delta_time
             if self._spawn_timer > self._next_spawn_time:
@@ -131,8 +159,10 @@ class GameView(arcade.Window):
                 obs.center_y = y
                 self.obstacle_list.append(obs)
 
-                if filename == "water.png":
-                    if self.difficulty < 2:
+                if filename == "bushes-single.png":
+                    if self.difficulty < 1.5:
+                        block_count = 1
+                    elif self.difficulty < 2.3:
                         block_count = random.randint(1, 3)
                     elif self.difficulty < 3:
                         block_count = random.randint(2, 4)
@@ -144,7 +174,16 @@ class GameView(arcade.Window):
                     print("blocks:", block_count)
 
                     for i in range(block_count):
-                        obs = arcade.Sprite("images/obstacles/water.png", scale=scale)
+                        if block_count == 1:
+                            texture = "images/obstacles/bushes-single.png"
+                        elif i == 0:
+                            texture = "images/obstacles/bushes-start.png"
+                        elif i == block_count - 1:
+                            texture = "images/obstacles/bushes-end.png"
+                        else:
+                            texture = "images/obstacles/bushes-middle.png"
+
+                        obs = arcade.Sprite(texture, scale=scale)
                         obs.center_x = WINDOW_WIDTH + 50 + (obs.width * i)
                         obs.center_y = y
                         self.obstacle_list.append(obs)
@@ -158,17 +197,42 @@ class GameView(arcade.Window):
             for obs in self.obstacle_list:
                 obs.center_x -= TILE_SPEED * self.difficulty
 
+            # platform generation
             for platform in self.platform_list:
                 platform.center_x -= TILE_SPEED * self.difficulty
 
-            # check leftmost platform
-            rightmost = max(p.right for p in self.platform_list)
+            rightmost_platform = max(p.right for p in self.platform_list)
             for platform in self.platform_list:
                 if platform.right < 0:
-                    platform.left = rightmost
-                    rightmost = platform.right
+                    platform.left = rightmost_platform
+                    rightmost_platform = platform.right
 
+            # cloud generation
+            for cloud in self.cloud_list:
+                cloud.center_x -= CLOUD_SPEED * self.difficulty
+
+            if self.cloud_list:
+                rightmost_cloud = max(c.right for c in self.cloud_list)
+                for cloud in self.cloud_list:
+                    if cloud.right < 0:
+                        cloud.left = rightmost_cloud + random.randint(300, 800)
+                        rightmost_cloud = cloud.right
+
+            # house generation
+            for house in self.house_list:
+                house.center_x -= CLOUD_SPEED * self.difficulty
+
+            if self.house_list:
+                rightmost_house = max(h.right for h in self.house_list)
+                for house in self.house_list:
+                    if house.right < 0:
+                        # move house to the right of the current rightmost house with random spacing
+                        house.left = rightmost_house + random.randint(200, 400)  # smaller spacing than clouds
+                        rightmost_house = house.right
+
+            # check collisions
             collided = arcade.check_for_collision_with_list(self.player_sprite, self.obstacle_list)
+
             if collided:
                 self.game_over = True
 
